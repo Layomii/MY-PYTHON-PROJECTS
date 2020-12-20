bea4acc529fe97f0519a76ef9daef7548eefd31c
Adetola Jesulayomi <69776053+Layomii@users.noreply.github.com>
2020-12-20 22:32:20 +0100

    Add files via upload

---
+++ contactbookpoject.py
@@ -0,0 +1,18 @@
+#dictionary has been stored in contacts
+contacts = {'Jesulayomi': '09022183739', 'Tofunmi': '08144174291', 'Sunmbo': '08035667878'}
+
+while True:
+    print('Enter a name:(blank to quit)')
+    name = input()
+    if name == '':
+        break
+
+#check to see if the key entered is in the dictionary
+    if name in contacts:
+        print(contacts + 'is the number of' + contacts[name])
+        else:
+            print('I do not have contact information for' + name)
+            print('What is their contact?')
+            cnts = input()
+            contacts[name] = cnts
+            print('Contact List Updated.')
